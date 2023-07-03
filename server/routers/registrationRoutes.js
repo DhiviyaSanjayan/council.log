68f2842fd1fa887ed1415613821eb784c7de7cf0
amalburhan <amalbrhn@gmail.com>
2023-07-03 17:02:05 +0100

    Commit changes

--- server/routers/registrationRoutes.js
+++ server/routers/registrationRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const RegistrationController = require('../controllers/RegistrationController');
+const authenticator = require('../middleware/authenticator');
 
 const router = express.Router();
 
-router.get('/', RegistrationController.getAllRegistrations);
+router.get('/', authenticator, RegistrationController.getAllRegistrations);
 router.get('/:id', RegistrationController.getRegistrationById);
 router.post('/', RegistrationController.createRegistration);
 router.put('/:id', RegistrationController.updateRegistration);
