b9cd559f36ade6c7697246147639ea11d3f56124
elliotclowes <elliot@clowes.me>
2023-07-05 11:37:30 +0100

    Updated tests as can now update user email too

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg')
 
 console.log(process.env.DB_URL);
 const db = new Pool({
-	connectionString: "postgres://pbozhwnh:6qvl3emysp9vDFm3LpNfFxvp66deEbEC@tyke.db.elephantsql.com/pbozhwnh"
+    connectionString: process.env.DB_URL
 })
 
 
